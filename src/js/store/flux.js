e9e4fc462df24bf58c9ebfb90b88dbe45a3addb1
Gabrielviscio10 <gonzalo.viscio@icloud.com>
2024-08-23 10:57:16 +0000

    agendas con fetch

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,50 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+    return {
+        store: {
+            agendas: [],  // Inicializar agendas como un array vacío
+            demo: [
+                {
+                    title: "FIRST",
+                    background: "white",
+                    initial: "white"
+                },
+                {
+                    title: "SECOND",
+                    background: "white",
+                    initial: "white"
+                }
+            ]
+        },
+        actions: {
+            exampleFunction: () => {
+                getActions().changeColor(0, "green");
+            },
+            loadSomeData: () => {
+                fetch('https://playground.4geeks.com/contact/agendas?offset=0&limit=100', {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                })
+                .then(resp => resp.json())
+                .then(data => {
+                    console.log(data); // Verifica que la estructura de data es la esperada
+                    setStore({ agendas: data.agendas });  // Acceder al array agendas dentro del objeto data
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            },
+            changeColor: (index, color) => {
+                const store = getStore();
+                const demo = store.demo.map((elm, i) => {
+                    if (i === index) elm.background = color;
+                    return elm;
+                });
+                setStore({ demo: demo });
+            }
+        }
+    };
 };
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,29 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+    const { store, actions } = useContext(Context);
+
+    useEffect(() => {
+        actions.loadSomeData();  // Cargar las agendas cuando el componente se monta
+    }, []);
+
+    return (
+        <div className="text-center mt-5">
+            <h1>Agendas</h1>
+            <ul>
+                {Array.isArray(store.agendas) && store.agendas.length > 0 ? (
+                    store.agendas.map((agenda, index) => (
+                        <li key={index}>{agenda.slug}</li> // Mostrar el slug de cada agenda
+                    ))
+                ) : (
+                    <p>No hay agendas disponibles.</p>
+                )}
+            </ul>
+            <a href="#" className="btn btn-success">
+                If you see this green button, bootstrap is working
+            </a>
+        </div>
+    );
+};
